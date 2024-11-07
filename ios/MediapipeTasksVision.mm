0ec71a1db42cdb92a6a1d565954b9147f2de51a0
mhp23 <hpousty@gmail.com>
2024-11-07 22:48:09 +0330

    style: cleanup the default initialization

--- ios/MediapipeTasksVision.mm
+++ ios/MediapipeTasksVision.mm
@@ -5,11 +5,7 @@ RCT_EXPORT_MODULE()
 
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
-- (NSNumber *)multiply:(double)a b:(double)b {
-    NSNumber *result = @(a * b);
 
-    return result;
-}
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-const MediapipeTasksVision = require('./NativeMediapipeTasksVision').default;
-
-export function multiply(a: number, b: number): number {
-  return MediapipeTasksVision.multiply(a, b);
-}
